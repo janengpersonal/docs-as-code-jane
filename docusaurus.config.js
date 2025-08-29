6cdca7458289ba38bfb5a24ec8b9ff10a61787bf
janengpersonal <tamthaonguyen.hcm@gmail.com>
2025-08-29 10:15:40 +0700

    Initial Docusaurus site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://janengpersonal.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '//docs-as-code-jane/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'janengpersonal', // Usually your GitHub org/user name.
+  projectName: 'docs-as-code-jane', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
